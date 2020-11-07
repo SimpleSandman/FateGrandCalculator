c6e321313ebaa8fbc6f24d72ecf9c57876a262e4
SimpleSandman <simple.sandman@gmail.com>
2020-11-07 08:12:24 -0600

    Set JSON files to "Copy if newer" and made ServantFilePath()

--- FateGrandOrderPOC.Test/CombatFormulaTest.cs
+++ FateGrandOrderPOC.Test/CombatFormulaTest.cs
@@ -1,10 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 using FateGrandOrderPOC.Shared.AtlasAcademy;
 using FateGrandOrderPOC.Shared.AtlasAcademy.Json;
 
 using FluentAssertions;
+
+using Newtonsoft.Json;
 
 using WireMock.RequestBuilders;
 using WireMock.ResponseBuilders;
@@ -27,8 +32,26 @@ namespace FateGrandOrderPOC.Test
         public async Task ReplaceThisWithRealTesting()
         {
             _wiremockFixture.CheckIfMockServerInUse();
+            
+            // Set "Copy to Output Directory" to "Copy if newer" for JSON files
+            string fullPath = ServantFilePath("JP", "Caster", "500300-TamamoNoMaeCasterEN.json");
+            ServantNiceJson testServant = JsonConvert.DeserializeObject<ServantNiceJson>(File.ReadAllText(fullPath));
 
             // do some testing here
         }
+
+        #region Private Methods
+        /// <summary>
+        /// Get the full file path for the servant data
+        /// </summary>
+        /// <param name="region">NA or JP</param>
+        /// <param name="className">AlterEgo, Archer, Assassin, Avenger, Berserker, Caster, Foreigner, Lancer, MoonCancer, Rider, Ruler, Saber, or Shielder</param>
+        /// <param name="servantFilename">The name of the servant's JSON file</param>
+        /// <returns></returns>
+        private string ServantFilePath(string region, string className, string servantFilename)
+        {
+            return Path.Combine(Environment.CurrentDirectory, "Json", region, "ServantData", className, servantFilename);
+        }
+        #endregion
     }
 }
