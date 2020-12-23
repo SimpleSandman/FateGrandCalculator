0e680b1ede6ae12de6c9281c83057172311ab71b
SimpleSandman <simple.sandman@gmail.com>
2020-12-23 15:51:45 -0600

    Remove unused class

--- FateGrandCalculator/Models/EnemyCollectionServant.cs
+++
@@ -1,9 +0,0 @@
-﻿using FateGrandCalculator.AtlasAcademy.Json;
-
-namespace FateGrandCalculator.Models
-{
-    public sealed class EnemyCollectionServant : EnemyMob
-    {
-        public ServantNiceJson ServantInfo { get; private set; }
-    }
-}
